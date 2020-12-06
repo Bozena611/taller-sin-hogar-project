105b8159fe40ed4167f6dee795853bf265075f9b
Bozena611 <bozena.vuckovic@gmail.com>
2020-12-06 02:14:30 +0100

    new sections Homepage

--- client/src/pages/Home/Homepage.js
+++ client/src/pages/Home/Homepage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Blocks from '../../img/block-1-juntos.svg';
 import './Homepage.css';
+import '../../index.css';
 import Button from 'react-bootstrap/Button';
 import Venimos from '../../img/block-2-venimos.png';
 import Vamos from '../../img/block-2-vamos.png';
@@ -24,14 +25,14 @@ const Homepage = () => {
 			</section>
 			
 			<div>
-				<h5 className="descubre-btn">Descubre más</h5> 
+				<h2 className="descubre-btn">Descubre más</h2> 
 			</div>
 			
 			<section className="section2">
 				<div className="section2-container">
 					<img src={Blocks} alt="blocks"></img>
 					<div className="image-text">
-						<h2>Juntos somos <span>Transformarte</span> y tú también puedes serlo.</h2>
+						<h2>Juntos somos <span className="section2-h1">Transformarte</span> y tú también puedes serlo.</h2>
 					</div>
 				</div>
 			</section>
@@ -41,20 +42,20 @@ const Homepage = () => {
 				<div className="section3-container">
 					<div className="section3-div1">
 						<img src={Venimos} alt="manos"></img>
-						<h2>¿De dónde venimos?</h2>
-						<p>La iniciativa nace a través de metodologías diseñadas para la creación y desarrollo de emprendimientos y proyectos de impacto social.</p>
+						<h3>¿De dónde venimos?</h3>
+						<p>La iniciativa nace a través de metodologías diseñadas para la creación y desarrollo de emprendimientos y <span>proyectos de impacto social</span>.</p>
 					</div>
 					<div className="section3-div2">
 						<img src={Vamos} alt="vamos"></img>
-						<h2>¿A dónde vamos?</h2>
-						<p>Nuestro objetivo es proporcionar una formación y ocupación a las personas sin hogar a la vez que  proponemos al público general una actividad donde aprendan y se sensibilicen sobre este colectivo.</p>
+						<h3>¿A dónde vamos?</h3>
+						<p>Nuestro objetivo es <span>proporcionar una formación y ocupación a las personas sin hogar</span> a la vez que  proponemos al público general una actividad donde aprendan y se sensibilicen sobre este colectivo.</p>
 						<Button variant="dark" className="section3-btn">DESCUBRE EL TALLER</Button>
 					</div>
 				</div>
 			</section>
 							
 			<section className="section4">
-				<h3>¿Qué nos ha llevado hasta aquí?</h3>
+				<h2>¿Qué nos ha llevado hasta aquí?</h2>
 				<div className="section4-container">
 					<img src={Llevados} alt="llevados"></img>
 					<Button variant="dark" className="section4-btn">COMPARTE Y VISIBILIZA</Button>
--- client/src/pages/Home/Section5.js
+++ client/src/pages/Home/Section5.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import './Homepage.css';
+import './Section5.css';
 import Garrido from '../../img/ezequiel-garrido.png';
-import Subtract from '../../img/Subtract.png';
+import Podcast from '../../img/Subtract.png';
 import Button from 'react-bootstrap/Button';
 
 const Section5 = () => {
 	return(
 		<div className="section5">
-			<h3>¿Qué proponemos?</h3>
+			<h2>¿Qué proponemos?</h2>
 				<div className="section5-container">
 					<div className="section5-grid-item1">
 						<img src={Garrido} alt="ezequiel-garrido" className="img-ezequiel"></img>
 					</div>
 					<div className="section5-grid-item2">
-						<p>El taller Upcycling Lab es un espacio de colaboración en el que personas sin y con hogar pueden trabajar juntos para dar una nueva vida a objetos desechados.</p>
+						<p><span>El taller Upcycling Lab</span> es un espacio de colaboración en el que personas sin y con hogar pueden trabajar juntos para dar una nueva vida a objetos desechados.</p>
 					</div>
 					<div className="section5-grid-item3">
-						<img src={Subtract} alt="subtract"></img>
+						<img src={Podcast} alt="subtract"></img>
 					</div>
 					<div className="section5-grid-item4">
 						<Button variant="dark" className="section5-btn">DESCUBRE EL TALLER</Button>
