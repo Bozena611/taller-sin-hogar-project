c7b36ebe007b57fdb822a380a6da79fb8b53cfad
Bozena611 <bozena.vuckovic@gmail.com>
2020-12-09 00:11:33 +0100

    css fixes + some mobile elements

--- client/src/pages/Home/Homepage.js
+++ client/src/pages/Home/Homepage.js
@@ -17,6 +17,11 @@ import Podcast from '../../img/Subtract.png';
 import Transparentes from '../../img/somos-transparentes.svg';
 import Transparentessvg from '../../img/transparentes.svg';
 
+import Mask1 from '../../img/mask-group1.png';
+import Mask2 from '../../img/mask-group2.png';
+import Mask3 from '../../img/mask-group3.png';
+import Mask4 from '../../img/mask-group4.png';
+
 import Nosotros from '../../img/quienes-somos.png';
 import Fundacion from '../../img/fundacion.svg';
 
@@ -41,72 +46,92 @@ const Homepage = () => {
 						<div>
 							<Image src={Blocks} alt="blocks" fluid/>
 						</div>
-
+						<section className="section3">
 						<h2>El proyecto Transformarte</h2>
 
 						<Row className="mb-5">
 							<Col md={{ span: 6}} className="">
-
-							<Image src={Venimos} alt="manos" fluid/>
-
-							<h3>¿De dónde venimos?</h3>
-							<p>La iniciativa nace a través de metodologías diseñadas para la creación y desarrollo de emprendimientos y <span>proyectos de impacto social</span>.</p>
-
+								<Image src={Venimos} alt="manos" fluid/>
+								<h3>¿De dónde venimos?</h3>
+								<p>La iniciativa nace a través de metodologías diseñadas para la creación y desarrollo de emprendimientos y <span>proyectos de impacto social</span>.</p>
 							</Col>
 							<Col md={{ span: 6}} className="">
 							
-							<Image src={Vamos} alt="vamos" fluid/>
-							<h3>¿A dónde vamos?</h3>
-							<p>Nuestro objetivo es <span>proporcionar una formación y ocupación a las personas sin hogar</span> a la vez que  proponemos al público general una actividad donde aprendan y se sensibilicen sobre este colectivo.</p>
-							<Button variant="dark" className="section3-btn" fluid>DESCUBRE EL TALLER</Button>
+								<Image src={Vamos} alt="vamos" fluid/>
+								<h3>¿A dónde vamos?</h3>
+								<p>Nuestro objetivo es <span>proporcionar una formación y ocupación a las personas sin hogar</span> a la vez que  proponemos al público general una actividad donde aprendan y se sensibilicen sobre este colectivo.</p>
+								<Button variant="dark" className="section3-btn" fluid>DESCUBRE EL TALLER</Button>
 
 							</Col>
 						</Row>
+						</section>
 
 							<h2>¿Qué nos ha llevado hasta aquí?</h2>
 
 							<Image src={Llevados} alt="llevados" fluid/>
 							<Button variant="dark" className="section4-btn">COMPARTE Y VISIBILIZA</Button>
 
-							<hr className="b-line"></hr>
+							{/*<hr className="b-line"></hr>*/}
 											
-
+						<section className="section5">
 							<h2>¿Qué proponemos?</h2>
 
 							<Row className="mb-5">
 								<Col md={{ span: 6}} >
-
-								<Image src={Podcast} alt="subtract" />
-								<p><span>El taller Upcycling Lab</span> es un espacio de colaboración en el que personas sin y con hogar pueden trabajar juntos para dar una nueva vida a objetos desechados.</p>
-
+									<p><span>El taller Upcycling Lab</span> es un espacio de colaboración en el que personas sin y con hogar pueden trabajar juntos para dar una nueva vida a objetos desechados.</p>
+									<Image src={Podcast} alt="subtract" className="podcast-green" />
 								</Col>
 								<Col md={{ span: 6}} >
 								
-								<Image src={Garrido} alt="ezequiel-garrido" className="img-ezequiel" />
-								<Button variant="dark" className="section5-btn">DESCUBRE EL TALLER</Button>
+									<Image src={Garrido} alt="ezequiel-garrido" className="img-ezequiel" />
+								{/*====BUTTON NEEDS TO BE HIDDEN ON MOBILE VERSION*/}
+									<Button variant="dark" className="section5-btn">DESCUBRE EL TALLER</Button>
 
 								</Col>
 							</Row>
-
-
+						</section>
+						<section className="section6">
 							<h2>Nuestra finalidad no es lucrativa</h2>
 							<div className="somos-transparentes">
-							<Image src={Transparentes} alt="transparentes" fluid/>
+							<Image src={Transparentes} className="section6-image" alt="transparentes" fluid/>
 								<div className="centered">
-								<Image src={Transparentessvg} alt="logo" fluid/>
+								<Image src={Transparentessvg} className="section6-somos-svg" alt="logo" fluid/>
+									{/*==== HAS TO APPEAR ON MOBILE VERSION, NOT WORKING====*/}
+									<h2 className="somos-mobile">Somos Transparentes</h2>
 									<p>Cuentas claras e ingresos justificados. Compruéba que todas las cifras cuadran descargando el documento del balance anual.</p>
 									<div className="section6-button">
 										<Button variant="dark" className="s6-btn">¿DÓNDE VA TU DINERO?</Button>
 									</div>
 								</div>
 							</div>
-														
+						</section>
+						<section className="section7">								
 							<h2>¿Quiénes estamos detrás?</h2>
+							
+							<Row className="mb-5">
+								<Col md={{ span: 6}} className="">
+									<Image src={Mask1} alt="mask1" fluid />
+								</Col>
+								<Col md={{ span: 6}} className="">
+									<Image src={Mask3} alt="mask3" fluid />
+								</Col>
+							</Row>
+							
+							<Row className="mb-5">
+								<Col md={{ span: 6}} className="">
+									<Image src={Mask4} alt="mask4" fluid />
+								</Col>
+								<Col md={{ span: 6}} className="">
+									<Image src={Mask2} alt="mask2" fluid />
+								</Col>
+						</Row>
+
 							<div>
 								<Image src={Nosotros} alt="transparentes" fluid />
 								<Image src={Fundacion} alt="transparentes" className="fundacion" fluid/>
 							</div>
-											
+						</section>	
+						<section className="section8">
 							<h2>Nuestro manifiesto</h2>
 							<hr className="b-line"/>
 							<div className="manif-flex">
@@ -143,7 +168,7 @@ const Homepage = () => {
 								<h1>Sostenibilidad, impacto social y economía creativa</h1>
 							</div>
 							<hr className="b-line"/>
-
+						</section>
 					</Col>
 					<Col md={{ span: 1 }}>
 						
