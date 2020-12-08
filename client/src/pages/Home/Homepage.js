711183898b7f9f2bd006b5391607f4b6049a756a
Misterlinux <mrlzarate8@gmail.com>
2020-12-08 07:52:55 +0100

    Angelo Zarate did the responsive

--- client/src/pages/Home/Homepage.js
+++ client/src/pages/Home/Homepage.js
@@ -6,14 +6,21 @@ import Button from 'react-bootstrap/Button';
 import Venimos from '../../img/block-2-venimos.png';
 import Vamos from '../../img/block-2-vamos.png';
 import Llevados from '../../img/text-block-3-llevado.svg';
-import Section5 from './Section5';
-import Section6 from './Section6';
-import Section7 from './Section7';
-import Section8 from './Section8';
 
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
+
+import Garrido from '../../img/ezequiel-garrido.png';
+import Podcast from '../../img/Subtract.png';
+
+import Transparentes from '../../img/somos-transparentes.svg';
+import Transparentessvg from '../../img/transparentes.svg';
+
+import Nosotros from '../../img/quienes-somos.png';
+import Fundacion from '../../img/fundacion.svg';
+
+import Yellow1 from '../../img/rectangle187.png';
 
 const Homepage = () => {
 	return(
@@ -51,7 +58,7 @@ const Homepage = () => {
 							<Image src={Vamos} alt="vamos" fluid/>
 							<h3>¿A dónde vamos?</h3>
 							<p>Nuestro objetivo es <span>proporcionar una formación y ocupación a las personas sin hogar</span> a la vez que  proponemos al público general una actividad donde aprendan y se sensibilicen sobre este colectivo.</p>
-							<Button variant="dark" className="section3-btn">DESCUBRE EL TALLER</Button>
+							<Button variant="dark" className="section3-btn" fluid>DESCUBRE EL TALLER</Button>
 
 							</Col>
 						</Row>
@@ -62,14 +69,80 @@ const Homepage = () => {
 							<Button variant="dark" className="section4-btn">COMPARTE Y VISIBILIZA</Button>
 
 							<hr className="b-line"></hr>
+											
 
-							<Section5 className="section" />
-											
-							<Section6 className="section" />
+							<h2>¿Qué proponemos?</h2>
+
+							<Row className="mb-5">
+								<Col md={{ span: 6}} >
+
+								<Image src={Podcast} alt="subtract" />
+								<p><span>El taller Upcycling Lab</span> es un espacio de colaboración en el que personas sin y con hogar pueden trabajar juntos para dar una nueva vida a objetos desechados.</p>
+
+								</Col>
+								<Col md={{ span: 6}} >
+								
+								<Image src={Garrido} alt="ezequiel-garrido" className="img-ezequiel" />
+								<Button variant="dark" className="section5-btn">DESCUBRE EL TALLER</Button>
+
+								</Col>
+							</Row>
+
+
+							<h2>Nuestra finalidad no es lucrativa</h2>
+							<div className="somos-transparentes">
+							<Image src={Transparentes} alt="transparentes" fluid/>
+								<div className="centered">
+								<Image src={Transparentessvg} alt="logo" fluid/>
+									<p>Cuentas claras e ingresos justificados. Compruéba que todas las cifras cuadran descargando el documento del balance anual.</p>
+									<div className="section6-button">
+										<Button variant="dark" className="s6-btn">¿DÓNDE VA TU DINERO?</Button>
+									</div>
+								</div>
+							</div>
 														
-							<Section7 className="section7" />
+							<h2>¿Quiénes estamos detrás?</h2>
+							<div>
+								<Image src={Nosotros} alt="transparentes" fluid />
+								<Image src={Fundacion} alt="transparentes" className="fundacion" fluid/>
+							</div>
 											
-							<Section8 className="section8" />
+							<h2>Nuestro manifiesto</h2>
+							<hr className="b-line"/>
+							<div className="manif-flex">
+								<div className="manif-unite">
+									<Image src={Yellow1} alt="mision" fluid/>
+									<div className="centrado">
+										<p>MISIÓN</p>
+									</div>
+								</div>
+								<div>
+									<h1>Sensibilizar</h1>
+								</div>
+							</div>
+							<hr className="b-line"/>
+							<div className="manif-flex">
+								<div className="manif-unite">
+									<Image src={Yellow1} alt="vision" fluid/>
+									<div className="centrado">
+										<p>VISIÓN</p>
+									</div>
+								</div>
+								<div>
+									<h1>Transformar</h1>
+								</div>
+							</div>
+							<hr className="b-line"/>
+							<div className="manif-flex">
+								<div className="manif-unite">
+									<Image src={Yellow1} alt="valores" className="valores" fluid/>
+									<div className="centrado">
+										<p>VALORES</p>
+									</div>
+								</div>
+								<h1>Sostenibilidad, impacto social y economía creativa</h1>
+							</div>
+							<hr className="b-line"/>
 
 					</Col>
 					<Col md={{ span: 1 }}>
