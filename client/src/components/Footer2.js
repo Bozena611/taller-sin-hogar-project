cbfb061e57219ec39d780687aa121b4e0eed3c87
Bozena611 <bozena.vuckovic@gmail.com>
2020-12-10 11:54:15 +0100

    hiding for mobile

---
+++ client/src/components/Footer2.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import './Footer.css';
+import Button from 'react-bootstrap/Button';
+
+const Footer2 = () => {
+	return (
+		<div className="footer-main">
+			<div className="footer-buttons">
+				<div>
+					<h4 className="footer-title">Y tú, ¿qué puedes hacer?</h4>
+				</div>
+				<div>
+					<Button variant="primary">DESCUBRE EL TALLER</Button>
+					{/*<button type="button" className="px-5 btn btn-outline-light btn-lg descubre">DESCUBRE EL TALLER</button>*/}
+				</div>
+				<div>
+					<Button variant="primary" className="compartir">COMPARTIR Y VISIBILIZAR</Button>
+					{/*<button type="button" className="px-5 btn btn-outline-light btn-lg compartir">COMPARTIR Y VISIBILIZAR</button>*/}
+				</div>
+			</div>
+			<hr className="hrz-line"/>
+			<div className="footer-flex1">
+				<div>
+					<p>Home</p>
+					<p>El equipo</p>
+					<p>Los valores</p>
+					<p>Transparencia</p>
+				</div>	
+				<div className="footer-upcycling">
+					<p>Upcycling Lab</p>
+					<p>Desarrollo</p>
+					<p>Precio</p>
+					<p>Testimonios</p>
+					<p>Vínculos</p>
+				</div>
+				<div className="footer-compra">
+						<p> Compra </p>
+						<p> Datos personales </p>
+						<p> Pagos </p>
+						<p> Info adicional </p>
+					</div>
+			</div>
+			<div>
+				<p className="footer-siguenos">Síguenos</p>
+				<div className="footer-flex2">
+					<p>icon1</p>
+					<p>icon2</p>
+					<p>icon3</p>
+				</div>
+			</div>	
+			<div>
+				<h5 className="transforma">TRANSFORMARTE</h5>
+			</div>
+			<p className="copyright">Copyright © 2020. LogoIpsum. All rights reserved.</p>
+		</div>
+	);
+}
+export default Footer2;
--- client/src/pages/Home/Homepage.js
+++ client/src/pages/Home/Homepage.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Blocks from '../../img/block-1-juntos.png';
+/*import Blocks from '../../img/block-1-juntos.png';*/
+import Blocks from '../../img/block1empty.svg';
 import './Homepage.css';
 import '../../index.css';
 import Button from 'react-bootstrap/Button';
@@ -7,6 +8,7 @@ import Venimos from '../../img/block-2-venimos.png';
 import Vamos from '../../img/block-2-vamos.png';
 import Llevados from '../../img/text-block-3-llevado.svg';
 
+import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Image from 'react-bootstrap/Image';
@@ -17,10 +19,10 @@ import Podcast from '../../img/Subtract.png';
 import Transparentes from '../../img/somos-transparentes.svg';
 import Transparentessvg from '../../img/transparentes.svg';
 
-import Mask1 from '../../img/mask-group1.png';
-import Mask2 from '../../img/mask-group2.png';
-import Mask3 from '../../img/mask-group3.png';
-import Mask4 from '../../img/mask-group4.png';
+import Mask1 from '../../img/mask1.png';
+import Mask2 from '../../img/mask2.png';
+import Mask3 from '../../img/mask3.png';
+import Mask4 from '../../img/mask4.png';
 
 import Nosotros from '../../img/quienes-somos.png';
 import Fundacion from '../../img/fundacion.svg';
@@ -29,10 +31,14 @@ import Yellow1 from '../../img/rectangle187.png';
 
 const Homepage = () => {
 	return(
-		<div className="Homepage">
-
+		
+			
+			
+		/*	<Container fluid>
 				<Row>
-					<Col md={{ span: 11}} className="">
+					<Col md={{ span: 11}} className="">*/
+
+					<div className="Homepage">
 						<h1 className="section">
 							Somos personas unidas por un proyecto social que responde a la falta 
 							de vínculos entre personas con y sin hogar mediante  talleres 
@@ -40,22 +46,34 @@ const Homepage = () => {
 						</h1>
 
 						<div>
-							<h2 className="descubre-btn mb-4">Descubre más</h2> 
+							<h2 className="descubre-btn">Descubre más</h2> 
 						</div>
 
+						<section className="section2">
+							<div className="section2-container">
+								<Image src={Blocks} alt="blocks" fluid />
+								<div className="image-text">
+									<h2>Juntos somos <span className="section2-h1">Transformarte</span> y tú también puedes serlo.</h2>
+								</div>
+							</div>
+						</section>
+						{/*
 						<div>
 							<Image src={Blocks} alt="blocks" fluid/>
-						</div>
+						</div>*/}
+
 						<section className="section3">
 						<h2>El proyecto Transformarte</h2>
-
+						<Container>
 						<Row className="mb-5">
-							<Col md={{ span: 6}} className="">
-								<Image src={Venimos} alt="manos" fluid/>
-								<h3>¿De dónde venimos?</h3>
-								<p>La iniciativa nace a través de metodologías diseñadas para la creación y desarrollo de emprendimientos y <span>proyectos de impacto social</span>.</p>
+							<Col xs={12} md={6} className="">
+								<div>
+									<Image src={Venimos} alt="manos" fluid/>
+									<h3>¿De dónde venimos?</h3>
+									<p>La iniciativa nace a través de metodologías diseñadas para la creación y desarrollo de emprendimientos y <span>proyectos de impacto social</span>.</p>
+								</div>
 							</Col>
-							<Col md={{ span: 6}} className="">
+							<Col xs={12} md={6} className="">
 							
 								<Image src={Vamos} alt="vamos" fluid/>
 								<h3>¿A dónde vamos?</h3>
@@ -64,13 +82,14 @@ const Homepage = () => {
 
 							</Col>
 						</Row>
+						</Container>
 						</section>
 
 							<h2>¿Qué nos ha llevado hasta aquí?</h2>
-
+							
 							<Image src={Llevados} alt="llevados" fluid/>
 							<Button variant="dark" className="section4-btn">COMPARTE Y VISIBILIZA</Button>
-
+							
 							{/*<hr className="b-line"></hr>*/}
 											
 						<section className="section5">
@@ -107,33 +126,57 @@ const Homepage = () => {
 						</section>
 						<section className="section7">								
 							<h2>¿Quiénes estamos detrás?</h2>
-							
-							<Row className="mb-5">
-								<Col md={{ span: 6}} className="">
-									<Image src={Mask1} alt="mask1" fluid />
-								</Col>
-								<Col md={{ span: 6}} className="">
-									<Image src={Mask3} alt="mask3" fluid />
-								</Col>
+						
+							<Container fluid className="section7-grid-mob">
+								<Row className="">
+									<Col md={{ span: 6}} className="">
+										<Image src={Mask1} alt="mask1" fluid className="mask1"/>
+									</Col>
+									<Col md={{ span: 6}} className="">
+										<Image src={Mask3} alt="mask3" fluid className="mask3"/>
+									</Col>
+								</Row>
+								
+								<Row className="mb-5">
+									<Col md={{ span: 6}} className="">
+										<Image src={Mask4} alt="mask4" fluid className="mask4"/>
+									</Col>
+									<Col md={{ span: 6}} xs={{ order: 'first' }} className="">
+										<Image src={Mask2} alt="mask2" fluid className="mask2"/>
+									</Col>
 							</Row>
+						</Container>
+					
+					{/*
+						<div className="container">
+							<div className="">
+								<div className="">
+									<Image src={Mask1} alt="mask1" fluid className="mask1"/>
+								</div>
+								<div className="nospaceright">
+								<Image src={Mask4} alt="mask4" fluid className="mask4"/>
+									
+								</div>
+							</div>
 							
-							<Row className="mb-5">
-								<Col md={{ span: 6}} className="">
-									<Image src={Mask4} alt="mask4" fluid />
-								</Col>
-								<Col md={{ span: 6}} className="">
-									<Image src={Mask2} alt="mask2" fluid />
-								</Col>
-						</Row>
-
-							<div>
+							<div className="">
+								<div className="nospaceleft">
+									<Image src={Mask3} alt="mask3" fluid className="mask3"/>
+								</div>
+								<div className="">
+									<Image src={Mask2} alt="mask2" fluid className="mask2"/>
+								</div>
+							</div>
+						</div>*/}
+							<div className="section7-grid-desktop">
 								<Image src={Nosotros} alt="transparentes" fluid />
 								<Image src={Fundacion} alt="transparentes" className="fundacion" fluid/>
 							</div>
 						</section>	
+					
 						<section className="section8">
 							<h2>Nuestro manifiesto</h2>
-							<hr className="b-line"/>
+						<hr className="b-line"/>
 							<div className="manif-flex">
 								<div className="manif-unite">
 									<Image src={Yellow1} alt="mision" fluid/>
@@ -169,13 +212,9 @@ const Homepage = () => {
 							</div>
 							<hr className="b-line"/>
 						</section>
-					</Col>
-					<Col md={{ span: 1 }}>
-						
-					</Col>
-				</Row>
-
-		</div>
+					</div>
+					
+					
 	);
 }
 
